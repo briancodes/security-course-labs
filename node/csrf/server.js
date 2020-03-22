b786d3e81ed8cbd7b70844a0568c52d5aefbea40
briancodes <15702512+briancodes@users.noreply.github.com>
2020-03-22 20:31:03 +0000

    server.js sameSite: 'strict', updated evil.html with details only

--- node/csrf/server.js
+++ node/csrf/server.js
@@ -3,16 +3,18 @@ const session = require('express-session');
 const cors = require('cors');
 const app = express();
 
-app.use(session({
-  secret: 'secret key',
-  saveUninitialized: true,
-  resave: false,
-  cookie: {
-    sameSite: 'strict', // (lax || strict)
-    maxAge: 3600000
-   }
-}));
-app.use(cors({origin: 'http://127.0.0.1:8080'}));
+app.use(
+  session({
+    secret: 'secret key',
+    saveUninitialized: true,
+    resave: false,
+    cookie: {
+      sameSite: 'strict', // (lax || strict)
+      maxAge: 3600000
+    }
+  })
+);
+app.use(cors({ origin: 'http://127.0.0.1:8080' }));
 app.use(express.static('../angular/dist/csrf/'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -35,26 +37,26 @@ app.get('/logout', (req, res) => {
 app.get('/transfer/:receiver/:value', (req, res) => {
   if (!req.session.loggedUser) {
     console.log('Unauthorized GET!');
-    res.status(401).json({error: "Unauthorized!"});
+    res.status(401).json({ error: 'Unauthorized!' });
   } else {
     const sender = req.session.loggedUser;
     const receiver = req.params.receiver;
     const value = req.params.value;
     console.log(`[GET] Sending ${value}$ to ${receiver} from ${sender}!`);
-    res.status(200).json({ message: 'SUCCESS'});
+    res.status(200).json({ message: 'SUCCESS' });
   }
 });
 
 app.post('/transfer', (req, res) => {
   if (!req.session.loggedUser) {
     console.log('Unauthorized POST!');
-    res.status(401).json({error: "Unauthorized!"});
+    res.status(401).json({ error: 'Unauthorized!' });
   } else {
     const sender = req.session.loggedUser;
     const receiver = req.body.receiver;
     const amount = req.body.amount;
     console.log(`[POST] Sending ${amount}$ to ${receiver} from ${sender}!`);
-    res.status(200).json({ message: 'SUCCESS'});
+    res.status(200).json({ message: 'SUCCESS' });
   }
 });
 
